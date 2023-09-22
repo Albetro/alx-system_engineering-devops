ee58d06a5ede8061b17102edf64c45acb9c21a4d
Albetro <albertxang@gmail.com>
2023-09-23 00:55:18 +0300

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #This code will install the Flask package version 2.1.0 using the pip3 package manager.
 package { 'flask':
-  ensure   => installed,
+  ensure   => '2.1.0',
   provider => 'pip3',
-  version  => '2.1.0',
 }
